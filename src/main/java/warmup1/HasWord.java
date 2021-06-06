0c866ed53c6c2e0c21eceb062c09ed6f3646d883
jmskeoch <jmskeoch@yahoo.com>
2021-06-05 21:59:36 -0700

    implement HasWord

--- src/main/java/warmup1/HasWord.java
+++ src/main/java/warmup1/HasWord.java
@@ -1,2 +1,17 @@
-package warmup1;public class HasWord {
+package warmup1;
+
+public class HasWord {
+
+    public static int hasWord(String input) {
+        return input.indexOf("word");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(hasWord("word"));
+        System.out.println(hasWord("WORD"));
+        System.out.println(hasWord("helloWord"));
+        System.out.println(hasWord("word word"));
+        System.out.println(hasWord("hello"));
+        System.out.println(hasWord(""));
+    }
 }
